ca7177ce3e58bf18585ebef6826092f83bd88edf
Your Name <you@example.com>
2021-01-20 11:06:37 +0700

    update

--- testfunction.php
+++ testfunction.php
@@ -1,28 +1,55 @@
 <?php
 $x = 100 ;
 $y = 10 ;
-
+$operator = "+";
 function plus($x, $y)
 {
-    $z = $x + $y;
-    echo "x + y = ",$z;
+    $z =$x + $y;
+    echo "x + y = ",$z."<br>";
 }
 function sub($x, $y)
 {
     $z = $x - $y;
-    echo "x - y = ",$z ;
+    echo "x - y = ",$z."<br>" ;
 }
 function mul($x, $y)
 {
     $z = $x * $y;
-    echo "x * y = ",$z ;
+    echo "x * y = ",$z."<br>" ;
 }
 function div($x, $y)
 {
     $z = $x / $y;
-    echo "x / y = ",$z ;
+    echo "x / y = ",$z."<br>" ;
 }
-plus($x, $y);
-sub($x, $y);
-mul($x, $y);
-div($x, $y);
+/*
+if($operator == "+"){
+    plus($x,$y);
+}
+else if($operator == "-"){
+    minus($x,$y);
+}
+else if($operator == "*"){
+    mul($x,$y);
+}
+else if($operator == "/"){
+    div($x,$y);
+}
+*/
+switch ($operator){
+    case("+"):
+        plus($x,$y);
+        break;
+    case("-"):
+        sub($x,$y);
+        break;
+    case("*"):
+        mul($x,$y);
+        break;
+    case("/"):
+        div($x,$y);
+        break;
+    default:
+}
+?>
+
