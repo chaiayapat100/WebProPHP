24aacf527e0f4ba8677cd373e83c2196d66bb980
Your Name <you@example.com>
2021-01-20 10:26:22 +0700

    update

---
+++ static.php
@@ -0,0 +1,13 @@
+<?php
+function myTest()
+{
+    static $x = 0;
+    echo $x . " , ";
+    $x++;
+}
+
+myTest(); // output = 0  , x = 1
+
+myTest(); // output = 1  , x = 2
+
+myTest();// output = 2  , x = 3
