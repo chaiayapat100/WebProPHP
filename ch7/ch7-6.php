9a128c785a539023e600d023cd14f1e735822433
Your Name <you@example.com>
2021-02-10 11:47:31 +0700

    update

---
+++ ch7/ch7-6.php
@@ -0,0 +1,13 @@
+<?php
+Echo getcwd().'<br>';
+$dir = 'C:\xampp\htdocs\WebProPHP-1';
+$files = scandir($dir);
+foreach($files as $value){
+echo "$value<br>";
+}
+echo getcwd().'<br>';
+chdir("..\..\..\\");
+echo getcwd().'<br>';
+chdir("htdocs\\");
+echo getcwd();
+?>
